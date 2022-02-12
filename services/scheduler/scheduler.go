2e4b12a09f4bd0f3fbdb783a40434bd52a56cfd5
ashadi <ashadi.cc@gmail.com>
2022-02-13 05:33:53 +0700

    revert back to iterate loop

--- services/scheduler/scheduler.go
+++ services/scheduler/scheduler.go
@@ -7,14 +7,14 @@ import (
 	"xrate/services"
 )
 
-//Task is to hold task function
+// Task is to hold task function
 type TaskFn func(ctx context.Context) error
 
-//ISchedulerService base methods scheduler service interface
+// ISchedulerService base methods scheduler service interface
 type ISchedulerService interface {
 	services.Service
 
-	//Add adding task to scheduler by given task and delay time
+	// Add adding task to scheduler by given task and delay time
 	AddTask(t TaskFn, delay time.Duration)
 }
 
@@ -24,19 +24,19 @@ type task struct {
 }
 
 type schedulerService struct {
-	//buffer of task channel
+	// buffer of task channel
 	ch chan *task
 }
 
-//NewService returns new scheduler service
+// NewService returns new scheduler service
 func NewService() ISchedulerService {
 	return &schedulerService{
-		//create task buffer max 10 channel
+		// create task buffer max 10 channel
 		ch: make(chan *task, 10),
 	}
 }
 
-//AddTask implementing ISchedulerService.AddTask
+// AddTask implementing ISchedulerService.AddTask
 func (s *schedulerService) AddTask(fn TaskFn, delay time.Duration) {
 	t := &task{
 		fn:    fn,
@@ -46,29 +46,22 @@ func (s *schedulerService) AddTask(fn TaskFn, delay time.Duration) {
 	s.ch <- t
 }
 
-//Run implementing services.Service
+// Run implementing services.Service
 func (s *schedulerService) Run(ctx context.Context) error {
 	log.Println("Scheduler service started...")
-	chErr := make(chan error, 1)
-	go func() {
-		//wait until done
-		<-ctx.Done()
-		log.Println("scheduler:gracefully shutdown...")
-		//close all task channel
-		close(s.ch)
-		close(chErr)
-	}()
-
-	for t := range s.ch {
-		task := t
-		go s.createWorker(ctx, task)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case t := <-s.ch:
+			go s.createWorker(ctx, t)
+		default:
+			continue
+		}
 	}
-
-	//block until get close signal from chErr
-	return <-chErr
 }
 
-//createWorker create new worker by given task and delay
+// createWorker create new worker by given task and delay
 func (s *schedulerService) createWorker(ctx context.Context, t *task) {
 	for {
 		select {
